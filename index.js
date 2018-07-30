721693eb3667231c682ad5ac53deb532aacfdff7
samuel madeu <tsmadeu@icloud.com>
2018-07-30 13:12:37 +0100

    refactored resolvers

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const token = process.env.TOKEN
 const baseURL = `https://api.teller.io/accounts`
 
 // fetches data by url
-const GetData = id => {
+const getData = id => {
     let url = id ? `${baseURL}/${id}` : baseURL
 
     return axios
@@ -23,12 +23,13 @@ const GetData = id => {
 const typeDefs = gql`
     type Query {
         accounts: [Account!]!
+        account(id: ID!): Account
     }
 
     type Account {
         name: String
-        tellerId: String!
-        enrollmentId: String
+        id: ID!
+        enrollmentId: ID
         currency: String
         balance: Float
         bankCode: String
@@ -49,27 +50,8 @@ const typeDefs = gql`
 
 const resolvers = {
     Query: {
-        accounts: () => GetData(),
-    },
-
-    Account: {
-        name: ({ id }) => GetData(id).then(({ name }) => name),
-        tellerId: ({ id }) => GetData(id).then(({ enrollment_id }) => enrollment_id),
-        enrollmentId: ({ id }) => GetData(id).then(({ enrollment_id }) => enrollment_id),
-        currency: ({ id }) => GetData(id).then(({ currency }) => currency),
-        balance: ({ id }) => GetData(id).then(({ balance }) => balance),
-        bankCode: ({ id }) => GetData(id).then(({ bank_code }) => bank_code),
-        accountNumber: ({ id }) => GetData(id).then(({ account_number }) => account_number),
-        institution: ({ id }) => GetData(id).then(({ institution }) => institution),
-        links: ({ id }) => GetData(id).then(({ links }) => links),
-    },
-
-    Links: {
-        transactions: ({ transactions }) => transactions,
-        standingOrders: ({ standing_orders }) => standing_orders,
-        self: ({ self }) => self,
-        payments: ({ payments }) => payments,
-        payee: ({ payee }) => payee,
+        accounts: () => getData(),
+        account: (parent, { id }) => getData(id),
     },
 }
 
