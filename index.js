b2a1ac3cf50cbcbf7f768cfe15a68bbfd1a646a4
samuel madeu <tsmadeu@icloud.com>
2018-07-31 16:58:50 +0100

    added payee quiries

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const token = process.env.TOKEN
 const baseURL = `https://api.teller.io/accounts`
 
 // fetches data by url
-const getData = ({ id }) => {
-    let url = !id ? baseURL : `${baseURL}/${id}`
+const getData = id => {
+    let url = id ? `${baseURL}/${id}` : baseURL
 
     return axios
         .get(url, {
@@ -23,15 +23,15 @@ const getData = ({ id }) => {
         .catch(error => console.log(error))
 }
 
-const getDataByUrl = ({ route, id }) => {
-    let url = `${baseURL}/${id}/${route}`
+const getDataByUrl = (route, id) => {
+    let url = route ? `${baseURL}/${id}/${route}` : baseURL
     return axios
         .get(url, {
             // auth token required by teller api
             headers: { Authorization: `Bearer ${token}` },
         })
         .then(res => {
-            //console.log(res.data)
+            console.log(res.data)
             return res.data
         })
         .catch(error => console.log(error))
@@ -42,6 +42,7 @@ const typeDefs = gql`
         accounts: [Account!]!
         account(id: ID!): Account
         transactions(id: ID!): [Transactions!]!
+        payees(id: ID!): [Payees!]!
     }
 
     type Account {
@@ -50,8 +51,8 @@ const typeDefs = gql`
         enrollmentId: ID
         currency: String
         balance: Float
-        bankCode: String
-        accountNumber: Int
+        bank_code: String
+        account_number: Int
         institution: String
         links: Links
     }
@@ -75,13 +76,23 @@ const typeDefs = gql`
         amount: Float
     }
 
+    type Payees {
+        name: String
+        links: Links
+        id: ID!
+        bank_code: String
+        account_number: Int
+        reference: String
+    }
+
 `
 
 const resolvers = {
     Query: {
-        accounts: () => getData(),
-        account: (parent, { id }) => getData({ id }),
-        transactions: (parent, { id }) => getDataByUrl({ route: 'transactions', id }),
+        accounts: () => getDataByUrl(),
+        account: (parent, { id }) => getData(id),
+        transactions: (parent, { id }) => getDataByUrl('transactions', id),
+        payees: (parent, { id }) => getDataByUrl('payees', id),
     },
 }
 
